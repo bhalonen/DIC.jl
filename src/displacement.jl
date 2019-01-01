179beba9287cda6926ee26ad10cf9901007d9f4e
Brent Halonen <bhalonen@gmail.com>
2019-01-01 15:28:41 -0500

    eulerian visualization working, results wrong

--- src/displacement.jl
+++ src/displacement.jl
@@ -1,4 +1,3 @@
-
 function __init__()
     global sobol_seq = SobolSeq(2)
 end
@@ -13,12 +12,13 @@ function DIC_analysis(dic_input::DIC_Input)
     deformed_images = dic_input.images[2:end]
     initial_guess_u=zeros(length(dic_input.dic_run_params.u_model))
     initial_guess_v=zeros(length(dic_input.dic_run_params.v_model))
-    size(reference_image)==size(deformed_images[1]) || error("all input images must be the same size $(size(reference_image)) versus $(size(deformed_image))")
+    
     roi_samples=map(1:dic_input.dic_run_params.dic_setting.sample_count) do idx
-        (Int32(idx%(length(deformed_images))+1),get_sample_point(dic_input.roi))
+        (frame = Int32(idx%(length(deformed_images))+1),sample_point = get_sample_point(dic_input.roi))
     end
-    original_values = map(sample -> reference_image[sample[2][1],sample[2][2]].val,roi_samples)
+    original_values = map(sample -> reference_image[sample.sample_point].val,roi_samples)
     deformed_images_plain = map(deformed_images) do image
+            size(reference_image)==size(image) || error("all input images must be the same size $(size(reference_image)) versus $(size(image))")
             map(image) do px
                 Float32(px.val)
             end 
@@ -33,11 +33,10 @@ function DIC_analysis(dic_input::DIC_Input)
     @time result = optimize(cost_function, cat(dims=1,initial_guess_u,initial_guess_v), NelderMead())
     u_params=Optim.minimizer(result)[1:length(initial_guess_u)]
     v_params=Optim.minimizer(result)[length(initial_guess_u)+1:end]
-    DIC_Output(Polynomial(u_params,dic_input.dic_run_params.u_model),Polynomial(v_params,dic_input.dic_run_params.v_model),Lagrangian)
+    DIC_Output(Polynomial(u_params,dic_input.dic_run_params.u_model),Polynomial(v_params,dic_input.dic_run_params.v_model),dic_input.roi,Lagrangian)
 end
-
 function cost_function_builder( deformed_image_itps::Vector{<:AbstractInterpolation},
-                                roi_samples::Vector{Tuple{Int32,CartesianIndex{2}}},
+                                roi_samples::Vector{<:NamedTuple},
                                 dic_run_params::DIC_Run_Parameters, 
                                 polynomial_coeff::Vector{<:Real},
                                 time_table::Vector{<:Real}, 
@@ -45,16 +44,14 @@ function cost_function_builder( deformed_image_itps::Vector{<:AbstractInterpolat
     u = Polynomial(polynomial_coeff[1:length(dic_run_params.u_model)],dic_run_params.u_model)
     v = Polynomial(polynomial_coeff[length(dic_run_params.u_model)+1:end],dic_run_params.v_model)
     transformed_values = pmap(roi_samples) do sample
-        get_transformed_value(deformed_image_itps[sample[1]],sample[2], u, v, time_table[sample[1]+1])
+        get_transformed_value(deformed_image_itps[sample.frame], sample.sample_point, u, v, time_table[sample.frame+1])
     end
     1-cor(original_values,transformed_values)
 end 
 function get_transformed_point(u::Polynomial,v::Polynomial,test_point::CartesianIndex{2},size_image::Tuple, time_value::Real)
     x_translation = u(test_point[1], test_point[2], time_value)
     y_translation = v(test_point[1], test_point[2], time_value)
-    x_point = max(1,min(x_translation + test_point[1], size_image[1]))
-    y_point = max(1,min(y_translation + test_point[2], size_image[2]))
-    return (x = x_point, y = y_point)
+    return position_bounded_to_image(x_translation + test_point[1], y_translation + test_point[2], size_image)
 end 
 function get_transformed_value(image_itp::AbstractInterpolation, sample::CartesianIndex{2}, u::Polynomial, v::Polynomial,time_value::Real) 
     point = get_transformed_point(u,v,sample, size(image_itp),time_value)
--- src/visualize.jl
+++ src/visualize.jl
@@ -1,6 +1,7 @@
 using Colors
 using FixedPointNumbers
 function get_color(color_range, maximum::Real, minimum::Real, value::Real)
+    maximum == minimum && return color_range[1]
     percentile = (value-minimum)/(maximum-minimum)
     return color_range[Int64(floor(percentile * (length(color_range)-1) )) + 1]
 end
@@ -13,17 +14,33 @@ function get_extrema(polynomial::Polynomial,roi::Rect_ROI,time_frame::Real)
     end
     extrema(values)
 end
+function get_red_green_color_range()
+    range(colorant"green",colorant"red")
+end 
 function make_heat_map(image::Matrix{T}, polynomial::Polynomial,roi::Rect_ROI, time_frame::Real) where T<:Gray
-    green = colorant"green"
-    red = colorant"red"
-    color_range = range(green,red)
+    color_range = get_red_green_color_range()
     color_image = map(px->RGB{N0f8}(px),image)
     x_range = roi.corner1.x:roi.corner2.x
     y_range = roi.corner1.y:roi.corner2.y
 
-    min_val,max_val = get_extrema(polynomial,roi,time_frame)
+    @show min_val,max_val = get_extrema(polynomial,roi,time_frame)
     for idxX in x_range, idxY in y_range
         color_image[idxX,idxY]=weighted_color_mean(.5,color_image[idxX,idxY],get_color(color_range,min_val,max_val, polynomial(idxX,idxY,time_frame)) )
     end
     return color_image
 end 
+function make_heat_map(image::Matrix{T}, polynomial::Polynomial, time_frame::Real, result::DIC_Output) where T<:Gray
+    roi = result.roi
+    color_range = get_red_green_color_range()
+    color_image = map(px->RGB{N0f8}(px),image)
+    x_range = roi.corner1.x:roi.corner2.x
+    y_range = roi.corner1.y:roi.corner2.y
+
+    @show min_val,max_val = get_extrema(polynomial,roi,time_frame)
+    for idxX in x_range, idxY in y_range
+        new_position = position_bounded_to_image(position(idxX, idxY, time_frame, result)..., size(image))
+        new_indexes = (x=Int(round(new_position.x)), y=Int(round(new_position.y)))
+        color_image[new_indexes.x,new_indexes.y]=weighted_color_mean(.5,color_image[new_indexes.x,new_indexes.y],get_color(color_range,min_val,max_val, polynomial(idxX,idxY,time_frame)) )
+    end
+    return color_image
+end
